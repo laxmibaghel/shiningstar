f9a3192719f230d8478507f3c811810121a12b48
Laxmi Devi <40895799+laxmibaghel@users.noreply.github.com>
2024-07-17 11:53:41 +0000

    push alll files

--- feed.py
+++ feed.py
@@ -11,6 +11,18 @@ with open('feed.yaml','r') as file:
 
 channel_element = xml_tree.SubElement(rss_element,'channel')
 xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+xml_tree.SubElement(channel_element,'format').text=yaml_data['format']
+xml_tree.SubElement(channel_element,'subtitle').text=yaml_data['subtitle']
+xml_tree.SubElement(channel_element,'itunes:author').text=yaml_data['author']
+xml_tree.SubElement(channel_element,'description').text=yaml_data['description']
+xml_tree.SubElement(channel_element,'itunes:image').text=yaml_data['image']
+xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+xml_tree.SubElement(channel_element,'title').text=yaml_data['title']
+
+
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
